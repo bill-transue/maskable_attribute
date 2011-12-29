c0158d5d185b13efaaaee96eb5d1ddfd522111c9
Bill Transue <transue@gmail.com>
2011-12-29 11:59:01 -0500

    Add Rdoc Comment to maskable_attribute Method

--- lib/maskable_attribute/acts_as_maskable_attribute.rb
+++ lib/maskable_attribute/acts_as_maskable_attribute.rb
@@ -6,6 +6,14 @@ module MaskableAttribute
     end
 
     module ClassMethods
+      ##
+      # Specifices an attribute to be masked, followed by masks to be made available to the attribute.
+      #
+      # ==== Examples
+      #
+      # class Foo < ActiveRecord::Base
+      #   maskable_attrribute :some_attribute, :some_method_be_used_as_a_mask, :another_attribute_mask
+      # end
       def maskable_attribute(masked_attribute, *masks)
         cattr_accessor :masked_attributes, :masks
         self.masked_attributes ||= Hash.new
