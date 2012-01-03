3266d761a830ddf0990e845681e0b49d97dca0b0
Bill Transue <transue@gmail.com>
2012-01-03 12:37:00 -0500

    Fix the Class Type Test in the Hickwell Test
    Test was checking for the wrong class.

--- test/dummy/test/unit/hickwell_test.rb
+++ test/dummy/test/unit/hickwell_test.rb
@@ -5,7 +5,12 @@ class HickwellTest < ActiveSupport::TestCase
     @hickwell = Hickwell.create!
   end
 
-  test "should have maskable_attribute foo" do
-    assert @hickwell.foo.is_a?(MaskableAttribute), "Masked attribute isn't a MaskableAttribute"
+  test "should have maskable_attribute qux" do
+    assert @hickwell.qux.is_a?(MaskableAttribute::MaskableAttribute), "Masked attribute isn't a MaskableAttribute"
+  end
+
+  test "should have getter and setter methods for qux" do 
+    assert_respond_to @hickwell, :qux,  "No getter method!"
+    assert_respond_to @hickwell, :qux=, "No setter method!"
   end
 end
