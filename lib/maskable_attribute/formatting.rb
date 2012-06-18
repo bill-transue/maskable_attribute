96ffcba455aa56e7284bd8b955267916ab32fd32
Bill Transue <transue@gmail.com>
2012-06-18 15:59:00 -0400

    Create New Classes Formats, Format Plus Tests
    This will (when finished) allow masks to have formats such as
    :capitalize, :upcase, or even Procs.

---
+++ lib/maskable_attribute/formatting.rb
@@ -0,0 +1,75 @@
+module MaskableAttribute
+  module Formatting
+    class Formats < Hash
+
+      #{ :format => :two_digit }
+      #{ :formats => [ :upcase, :downcase ] }
+      #{ :exclusive_format => { :capitalized => Proc.new{ |mask| mask.captialized } } }
+      #{ :exclusive_formats =>{ :capitalized => Proc.new{ |mask| mask.captialized }, :titleized => :titleize } }
+      #{ :default_format => :titleize }
+      attr_accessor :are_exclusive, :has_default, :default
+      def initialize(options = {})
+        @are_exclusive, @has_default = false, false
+        @default = nil
+        unless options.nil?
+          options.each do |type, formats|
+            if formats.is_a? Symbol or formats.is_a? Proc
+              self[formats] = Format.new formats
+            elsif formats.is_a? Array
+              formats.each do |format|
+                self[format] = Format.new format
+              end
+            else
+              formats.each do |format, method|
+                self[format] = Format.new method || format
+              end
+            end
+
+            if type.to_s.include? "exclusive"
+              @are_exclusive = true
+            elsif type.to_s.include? "default"
+              @has_default = true
+              @default = self[formats]
+            end
+          end
+        end
+      end
+
+      def apply(format, &block)
+        if block_given?
+          fetch(format, (has_default? ? @default : Format.new)).apply yield
+        else
+          fetch(format)
+        end
+      end
+
+      def names
+        keys
+      end
+
+      def are_exclusive?
+        @are_exclusive
+      end
+
+      def has_default?
+        @has_default
+      end
+    end
+
+    class Format
+      def initialize(method=nil)
+        @method = method
+      end
+
+      def apply(input)
+        if @method.is_a? Symbol
+          input.send(@method)
+        elsif @method.is_a? Proc
+          @method.call input
+        else
+          input
+        end
+      end
+    end
+  end
+end
---
+++ test/formatting_test.rb
@@ -0,0 +1,71 @@
+require 'test_helper'
+
+class FormattingTest < ActiveSupport::TestCase
+
+  #describe Format
+  test "should accept a symbol as the format method" do
+    format = MaskableAttribute::Formatting::Format.new :upcase
+    assert_equal "SUCCESS", format.apply("success")
+  end
+
+  test "should accept a Proc as the format method" do
+    format = MaskableAttribute::Formatting::Format.new Proc.new { |value| value.upcase }
+    assert_equal "SUCCESS", format.apply("success")
+  end
+
+  #describe Formats
+  test "should accept a hash of a single format" do
+    formats = MaskableAttribute::Formatting::Formats.new :formats => :upcase
+
+    assert_equal "SUCCESS", formats[:upcase].apply("success")
+  end
+
+  test "should accept a hash of an array of formats" do
+    formats = MaskableAttribute::Formatting::Formats.new :formats => [ :upcase, :downcase ]
+
+    assert_equal "SUCCESS", formats[:upcase].apply("success"), "First element not assigned as name/method"
+    assert_equal "success", formats[:downcase].apply("SUCCESS"), "Second element not assigned as name/method"
+  end
+
+  test "should accept a hash of a hash with a symbol as a value" do
+    formats = MaskableAttribute::Formatting::Formats.new :formats => { :upcased => :upcase }
+
+    assert_equal "SUCCESS", formats[:upcased].apply("success")
+  end
+
+  test "should accept a hash of a hash of formats" do
+    formats = MaskableAttribute::Formatting::Formats.new :formats => { :two_digit => Proc.new { |value| format '%02d', value },
+                                                                       :upcased => :upcase,
+                                                                       :downcase => nil }
+
+    assert_equal '01', formats[:two_digit].apply(1), "Proc not used as method"
+    assert_equal "SUCCESS", formats[:upcased].apply("success"), "Symbol not used as method"
+    assert_equal "success", formats[:downcase].apply("SUCCESS"), "Name, in leu of nil, not used as method"
+  end
+
+  test "should recognize exclusive formats" do
+    formats = MaskableAttribute::Formatting::Formats.new :exclusive_formats => [ :upcase, :downcase ]
+
+    assert formats.are_exclusive?, "Did not recognize exclusive formats"
+  end
+
+  test "should recognize a default_format" do
+    formats = MaskableAttribute::Formatting::Formats.new :default_format => :upcase
+
+    assert formats.has_default?
+    assert_equal formats[:upcase], formats.default, "Default format not assigned"
+    assert_equal 'SUCCESS', formats.default.apply("success"), "Default format not applied"
+  end
+
+  test "should be able to use a block for applying formats" do
+    formats = MaskableAttribute::Formatting::Formats.new :formats => :upcase
+
+    assert_equal 'SUCCESS', formats.apply(:upcase) { "success" }
+  end
+
+  test "should provide all format names" do
+    formats = MaskableAttribute::Formatting::Formats.new :formats => [ :upcase, :downcase ]
+
+    assert_equal [ :upcase, :downcase ], formats.names, "Did not provide all format names"
+  end
+end
